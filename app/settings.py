b69d05cdfb0fa121f4d93fcc482d22cd4a589f10
webvolant <barkalov_anton@mail.ru>
2014-05-04 21:02:45 +0200

    set

--- app/settings.py
+++ app/settings.py
@@ -36,7 +36,10 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'haupt',	
+    'haupt',
+    'bootstrap3',
+    'south'
+		
 )
 
 MIDDLEWARE_CLASSES = (
