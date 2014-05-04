1bc23741757c920fb4eab7d5db25c4005c5582f4
webvolant <barkalov_anton@mail.ru>
2014-05-04 16:08:00 +0200

    dbfile

--- app/settings.py
+++ app/settings.py
@@ -55,12 +55,15 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
